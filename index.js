431ce43f851745d4ed4a61d429f8f52bcc96a40e
starbuckshiker <u1304954@umail.utah.edu>
2020-11-23 19:34:58 -0700

    content

--- index.js
+++ index.js
@@ -22,34 +22,83 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), 
-        (err, content) => {
-            if (err) throw err;
-            res.writeHead(200, { 'Content-Type' : 'text/html'});
-            res.end(content);
-        }
-    );
-}
-
-// if (req.url === '/about') {
-//     fs.readFile
-//     (path.join(__dirname, 'public', 'about.html'),
-//      (err, content) => {
-//         if (err) throw err;
-//         res.writeHead(200, { 'Content-type' : 'text/html'});
-//         res.end(content);
-//     }
+//     if (req.url === '/') {
+//         fs.readFile(path.join(__dirname, 'public', 'index.html'), 
+//         (err, content) => {
+//             if (err) throw err;
+//             res.writeHead(200, { 'Content-Type' : 'text/html'});
+//             res.end(content);
+//         }
 //     );
-    if (req.url === '/api/user') {
-        const users = [
-            { name: 'I Rock', age: 40 },
-            { name: 'You Rock', age: 30 }
-        ];
-        res.writeHead(200, { 'Content-Type' : 'application/json'});
-        res.end(JSON.stringify(users));
+// }
 
-}
+// // if (req.url === '/about') {
+// //     fs.readFile
+// //     (path.join(__dirname, 'public', 'about.html'),
+// //      (err, content) => {
+// //         if (err) throw err;
+// //         res.writeHead(200, { 'Content-type' : 'text/html'});
+// //         res.end(content);
+// //     }
+// //     );
+//     if (req.url === '/api/user') {
+//         const users = [
+//             { name: 'I Rock', age: 40 },
+//             { name: 'You Rock', age: 30 }
+//         ];
+//         res.writeHead(200, { 'Content-Type' : 'application/json'});
+//         res.end(JSON.stringify(users));
+
+// }
+
+    // Build file path
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    
+    // Extension of File
+    let extname = path.extname(filePath);
+
+    // Initial content type
+    let contentType = 'text/html';
+
+    // Check ext and set content type
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application.json';
+            break;    
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;    
+    }
+
+    // Read File
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                // Page not found
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
+                    res.writeHead(200, { 'Content-Type' : 'text/html'});
+                    res.end(content, 'utf-8');
+                })
+            } else {
+                // some server error
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            // success
+            res.writeHead(200, { 'Content-Type' : 'text/html'});
+            res.end(content, 'utf-8');
+        }
+    });
 });
 
 
