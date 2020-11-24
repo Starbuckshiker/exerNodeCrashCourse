9f4179d01e57aa6c6cca39291762bb44bdce3faa
starbuckshiker <u1304954@umail.utah.edu>
2020-11-23 18:21:16 -0700

    logger

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //console.log("Hello from Node.js...")
-const Person = require("./person");
+// const Person = require("./person");
 
-const person1 = new Person("John Doe", 30);
+// const person1 = new Person("John Doe", 30);
 
-person1.greeting();
+// person1.greeting();
 
-console.log(Person);
+// console.log(Person);
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', data => console.log('Called Listener', data ));
+
+logger.log("Meow World");
+logger.log("Did you say Meow");
+logger.log("Meow");
